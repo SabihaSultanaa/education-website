783dbdf0998436bdad16d6908ed9e3425f8d903a
SABIHA SULTANA <160202245+SabihaSultanaa@users.noreply.github.com>
2024-11-10 04:18:53 +0500

    changes

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -4,7 +4,7 @@ function Feed(){
 
         <>
         <div className="bg-white">
-        <h1 className=" stu">What Student's Say</h1>
+        <h1 className=" stu">What Students Say</h1>
         <p className="stup">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, nesciunt! 
         </p>
         <div className="simg"></div>
